a8a9a5d61be07c1dca29d61c7a271f10de1e7851
anthonyarimalala <119404637+anthonyarimalala@users.noreply.github.com>
2025-01-09 10:27:40 +0300

    global-css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,22 +11,10 @@
     <title>Hpi Suivis</title>
     <!-- plugins:css -->
 
-    <link rel="stylesheet" href="{{ asset('star-admin2/vendors/feather/feather.css') }}">
     <link rel="stylesheet" href="{{ asset('star-admin2/vendors/mdi/css/materialdesignicons.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('star-admin2/vendors/ti-icons/css/themify-icons.css') }}">
-    <link rel="stylesheet" href="{{ asset('star-admin2/vendors/typicons/typicons.css') }}">
-    <link rel="stylesheet" href="{{ asset('star-admin2/vendors/simple-line-icons/css/simple-line-icons.css') }}">
-    <link rel="stylesheet" href="{{ asset('star-admin2/vendors/css/vendor.bundle.base.css') }}">
-    <!-- endinject -->
-    <!-- Plugin css for this page -->
-    <link rel="stylesheet" href="{{ asset('star-admin2/vendors/datatables.net-bs4/dataTables.bootstrap4.css') }}">
-    <link rel="stylesheet" href="{{ asset('star-admin2/js/select.dataTables.min.css') }}">
-    <!-- End plugin css for this page -->
-    <!-- inject:css -->
     <link rel="stylesheet" href="{{ asset('star-admin2/css/vertical-layout-light/style.css') }}">
-    <!-- endinject -->
-    <link rel="shortcut icon" href="" />
-    <script src="{{ asset('chart.js/Chart.min.js') }}"></script>
+    <link rel="stylesheet" href="{{ asset('star-admin2-global/template.css') }}">
+
 
 
 
@@ -177,25 +165,8 @@
 <!-- container-scroller -->
 
 <!-- plugins:js -->
-<script src="{{ asset('star-admin2/vendors/js/vendor.bundle.base.js') }}"></script>
-<!-- endinject -->
-<!-- Plugin js for this page -->
-<script src="{{ asset('star-admin2/vendors/chart.js/Chart.min.js') }}"></script>
-<script src="{{ asset('star-admin2/vendors/bootstrap-datepicker/bootstrap-datepicker.min.js') }}"></script>
-<script src="{{ asset('star-admin2/vendors/progressbar.js/progressbar.min.js') }}"></script>
-
-<!-- End plugin js for this page -->
-<!-- inject:js -->
-<script src="{{ asset('star-admin2/js/off-canvas.js') }}"></script>
-<script src="{{ asset('star-admin2/js/hoverable-collapse.js') }}"></script>
-<script src="{{ asset('star-admin2/js/template.js') }}"></script>
-<script src="{{ asset('star-admin2/js/settings.js') }}"></script>
-<script src="{{ asset('star-admin2/js/todolist.js') }}"></script>
-<!-- endinject -->
-<!-- Custom js for this page-->
 <script src="{{ asset('star-admin2/js/jquery.cookie.js') }}" type="text/javascript"></script>
-<script src="{{ asset('star-admin2/js/dashboard.js') }}"></script>
-<script src="{{ asset('star-admin2/js/Chart.roundedBarCharts.js') }}"></script>
+<script src="{{ asset('star-admin2-global/template.js') }}"></script>
 
 <script>
     document.addEventListener('DOMContentLoaded', function () {
